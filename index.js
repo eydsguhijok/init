f93dd3834dd32c0a5b0354279472716d544826b1
eydsguhijok <dsjcajdkasjdkaj@hotmail.com>
2024-05-14 04:33:23 +0700

    feat

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const gcdRecursive = (a, b) => {
-  if (b === 0) {
-    return a;
-  }
-  return gcdRecursive(b, a % b);
-};
+const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
