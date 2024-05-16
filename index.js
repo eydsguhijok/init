9878ea0bba86a548ad99071a0d2409732cbd8ab7
eydsguhijok <dsjcajdkasjdkaj@hotmail.com>
2024-05-17 01:12:54 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function findMin(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) left = mid + 1;
-    else if (nums[mid] < nums[right]) right = mid;
-    else right--;
+function binarySearch(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) low = mid + 1;
+    else high = mid - 1;
   }
-  return nums[left];
+  return -1;
 }
